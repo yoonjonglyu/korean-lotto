a488dcb0845e08b7242bc1cfe916dee889784536
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-04 22:29:11 +0900

    feat: REDUX DEFAULT SETTING

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hot  } from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 import App from './components/App';
 
+import { createStore } from 'redux';
+import reducers from './reducers'; 
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers);
 
 const render = (Component) => {
     ReactDOM.render(
-        <Component />,
+        <Provider store={store}>
+            <Component />
+        </Provider>,
         document.getElementById('app')
-        );
+    );
 }
 
 render(App);
