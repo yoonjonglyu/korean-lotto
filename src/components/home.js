c922dacf43c9a9a9f487f7e98b1100944ad1a870
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-05 22:05:17 +0900

    fix: HOME MARKUP - HTML

--- src/components/home.js
+++ src/components/home.js
@@ -7,18 +7,23 @@ import { connect } from 'react-redux';
 
 
 const Home = (props) => {
-  const lottoNumber = props.lotto[0] !== undefined?  props.lotto : [1, 2, 3, 4, 5, 6];
+  const lottoNumber = props.lotto.length > 0 ?  props.lotto : [1, 2, 3, 4, 5, 6];
 
   return (
     <Layout>
       <div className="home-contents">
-         <h1>로또</h1>
-         <div>
-           한강 수온 표
-           {props.hangang}
+         <h2>로또 추첨기</h2>
+         <div className="hangang-box">
+           <h2>한강 수온</h2>
+           <p>
+           謀事在人,成事在天(모사재인 성사재천)<br />
+           "일을 꾸미는 것은 사람이되 일을 이루게 하는 것은 하늘이어서 강제로 할 수가 없다."<br />
+           -제갈량-
+           </p>
+           <h3>{props.hangang} °C</h3>
          </div>
-         <div>
-           지난 회차 당첨번호
+         <div className="lotto-box">
+           <h2>지난 회차 당첨번호</h2>
            <ViewNumber number={lottoNumber}/>
          </div>
       </div>
