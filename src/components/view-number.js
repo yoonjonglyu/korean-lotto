5d0050a4c75d4b453bc0034d5422f2120b49b139
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-10-06 15:43:09 +0900

    fix: VEIW NUMBER BG COLOR & BONUS NUMBER STYLE

--- src/components/view-number.js
+++ src/components/view-number.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
 const viewNumber = ({ number } = props) => {
-    const ballList = number.map((num, index) => 
-        (<li key={index}>{num}</li>)
+    const ballList = number.map((num, index) => {
+        let color, bonus;
+
+        if(num >= 40){
+            color = "green";
+        } else if(num >= 30){
+            color = "gray";
+        } else if(num >= 20){
+            color = "red";
+        } else if(num >= 10){
+            color = "blue";
+        } else {
+            color = "orange";
+        }
+
+        if(index === 6){
+            bonus = ' bonus';
+        } else {
+            bonus = '';
+        }
+
+    return (<li key={index} className={color + (bonus)}>{num}</li>);
+    }
+        
     );
     
     return (
